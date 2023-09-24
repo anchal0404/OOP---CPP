174d52d7684eeea74c99c5429bd643351b366dd3
Anchal Rathore <76997901+anchal0404@users.noreply.github.com>
2023-09-24 15:19:42 +0530

    24/09/2023

--- static_members.cpp
+++ static_members.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Employee
 {
 	int id;
-	static int count;
+	static int count; //Static variable associated with class.
 
 	public:
 		void setData(void)
@@ -17,20 +17,26 @@ class Employee
 		{
 			cout<<"This id of this employee is "<<id<<" and this is employee number "<<count<<endl;
 		}
+
+		static void getCount(void)
+		{
+			cout<<"The value of count is "<<count<<endl;
+		}
 };
-int Employee :: count;
+int Employee :: count=10;
 int main(int argc, char const *argv[])
 {
 	Employee anshi,tria,kivra;
 
 	anshi.setData();
 	anshi.getData();
-
+	Employee:: getCount();
 	tria.setData();
 	tria.getData();
-
+	Employee::getCount;
 	kivra.setData();
 	kivra.getData();
+	Employee::getCount();//Access static function associated with class they only acccess static member of the class.
 
 
 	return 0;
